06f58c03e812fa6bd9c669fb4edd9b71b9de2ddc
Ewillian <guillaume.lecoq@ynov.com>
2020-03-20 15:54:13 +0100

    working kubedb with wordpress

--- RockStartConfig/Rockstar-With-KubeDB/scripts/init-wordpress-database.sql
+++ RockStartConfig/Rockstar-With-KubeDB/scripts/init-wordpress-database.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
 USE wordpress;
-ALTER USER 'wordpress'@'%' identified with mysql_native_password by 'wordpress';

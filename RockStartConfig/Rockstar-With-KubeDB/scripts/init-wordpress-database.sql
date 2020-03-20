11dbdb3a9f318ed9ccad376f518ba551a6697b4b
Ewillian <guillaume.lecoq@ynov.com>
2020-03-20 15:15:08 +0100

    wordpress <--> mysql ok but connection error

--- RockStartConfig/Rockstar-With-KubeDB/scripts/init-wordpress-database.sql
+++ RockStartConfig/Rockstar-With-KubeDB/scripts/init-wordpress-database.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER 'wordpress'@'%' IDENTIFIED BY 'wordpress';
 USE wordpress;
+ALTER USER 'wordpress'@'%' identified with mysql_native_password by 'wordpress';
